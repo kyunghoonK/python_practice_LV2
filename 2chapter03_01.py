caa5cfbcc428e6de035fdf6253026bb7156fb74b
kyunghoon <rlarudgns970@naver.com>
2022-03-28 15:36:06 +0900

    .

--- 2chapter03_01.py
+++ 2chapter03_01.py
@@ -21,3 +21,55 @@ print(n * 100, n.__mul__(100))
 
 print()
 print()
+
+# 클래스 예제1
+class Fruit:
+    def __init__(self, name, price):
+        self._name = name
+        self._price = price
+
+    def __str__(self):
+        return 'Fruit Class Info : {} , {}'.format(self._name, self._price)
+    
+    def __add__(self, x):
+        print('Called >> __add__')
+        return self._price + x._price
+
+    def __sub__(self, x):
+        print('Called >> __sub__')
+        return self._price - x._price
+    
+    def __le__(self, x):
+        print('Called >> __le__')
+        if self._price <= x._price:
+            return True
+        else:
+            return False
+    
+    def __ge__(self, x):
+        print('Called >> __ge__')
+        if self._price >= x._price:
+            return True
+        else:
+            return False
+
+
+
+
+
+# 인스턴스 생성
+s1 = Fruit('Orange', 7500)
+s2 = Fruit('Banana', 3000)
+
+print(s1 + s2)
+
+# 일반적인 계산
+# print(s1._price + s2._price)
+
+# 매직메소드
+print(s1 >= s2)
+print(s1 <= s2)
+print(s1 - s2)
+print(s2 - s1)
+print(s1)
+print(s2)
