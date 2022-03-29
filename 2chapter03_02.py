584e76d292dac39f80dcb7226bb1a8fc9e47baa6
kyunghoon <rlarudgns970@naver.com>
2022-03-29 22:13:44 +0900

    .

--- 2chapter03_02.py
+++ 2chapter03_02.py
@@ -9,11 +9,11 @@
 # Max((5,10)) = 10
 
 class Vector(object):
-    def __init__(self, *arg):
+    def __init__(self, *args):
         '''
         Create a vector, example : v = Vector(5, 10)
         '''
-        if len(arg) == 0:
+        if len(args) == 0:
             self._x, self._y = 0, 0
         else:
             self._x, self._y = args
@@ -27,7 +27,7 @@ class Vector(object):
         return Vector(self._x + other._x, self._y + other._y)
     
     def __mul__(self, y):
-        return Vector(self._x * y, self._y + y)
+        return Vector(self._x * y, self._y * y)
     
     def __bool__(self):
         return bool(max(self._x, self._y))
@@ -35,9 +35,15 @@ class Vector(object):
 # Vector 인스턴스 생성
 v1 = Vector(5,7)
 v2 = Vector(23, 35)
+v3 = Vector()
 
-
-
-
-
+# 매직메소드 출력
 print(Vector.__init__.__doc__)
+print(Vector.__repr__.__doc__)
+print(Vector.__add__.__doc__)
+print(v1, v2, v3)
+print(v1 + v2)
+print(v1 * 3)
+print(v2 * 10)
+print(bool(v1), bool(v2))
+print(bool(v3))
